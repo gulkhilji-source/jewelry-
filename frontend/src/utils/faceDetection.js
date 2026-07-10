aa9ea43155c14d99eef58831677fa1e2c82425ab
gulkhilji-source <gulerehmatkhilji@gmail.com>
2026-07-10 20:11:59 +0500

    Create faceDetection.js

---
+++ frontend/src/utils/faceDetection.js
@@ -0,0 +1,24 @@
+import {
+  FaceDetector,
+  FilesetResolver,
+} from "@mediapipe/tasks-vision";
+
+let detector = null;
+
+export async function getFaceDetector() {
+  if (detector) return detector;
+
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+  );
+
+  detector = await FaceDetector.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/latest/blaze_face_short_range.tflite",
+    },
+    runningMode: "IMAGE",
+  });
+
+  return detector;
+}
