b925f3520b96244360075737091c2c1faa3045d7
gulkhilji-source <gulerehmatkhilji@gmail.com>
2026-07-10 20:14:27 +0500

    Create jewelryRules.js

---
+++ frontend/src/utils/jewelryRules.js
@@ -0,0 +1,98 @@
+export const recommendations = {
+  Warm: {
+    Black: {
+      metal: "Gold",
+      necklace: "Gold Diamond Necklace",
+      earrings: "Gold Hoop Earrings",
+      bracelet: "18K Gold Bracelet",
+    },
+
+    Green: {
+      metal: "Gold",
+      necklace: "Emerald Pendant Necklace",
+      earrings: "Gold Emerald Earrings",
+      bracelet: "Gold Emerald Bracelet",
+    },
+
+    Red: {
+      metal: "Gold",
+      necklace: "Ruby Pendant Necklace",
+      earrings: "Ruby Stud Earrings",
+      bracelet: "Gold Bracelet",
+    },
+
+    Blue: {
+      metal: "Gold",
+      necklace: "Sapphire Necklace",
+      earrings: "Gold Sapphire Earrings",
+      bracelet: "Gold Bracelet",
+    },
+
+    White: {
+      metal: "Gold",
+      necklace: "Pearl Necklace",
+      earrings: "Pearl Earrings",
+      bracelet: "Gold Bracelet",
+    },
+  },
+
+  Cool: {
+    Black: {
+      metal: "Silver",
+      necklace: "Diamond Necklace",
+      earrings: "Silver Diamond Earrings",
+      bracelet: "Silver Bracelet",
+    },
+
+    Green: {
+      metal: "Silver",
+      necklace: "Emerald Necklace",
+      earrings: "Silver Emerald Earrings",
+      bracelet: "Silver Bracelet",
+    },
+
+    Blue: {
+      metal: "Silver",
+      necklace: "Sapphire Necklace",
+      earrings: "Silver Sapphire Earrings",
+      bracelet: "Silver Bracelet",
+    },
+
+    White: {
+      metal: "Platinum",
+      necklace: "Pearl Necklace",
+      earrings: "Pearl Earrings",
+      bracelet: "Platinum Bracelet",
+    },
+  },
+
+  Neutral: {
+    Black: {
+      metal: "Rose Gold",
+      necklace: "Diamond Necklace",
+      earrings: "Rose Gold Earrings",
+      bracelet: "Rose Gold Bracelet",
+    },
+
+    Green: {
+      metal: "Rose Gold",
+      necklace: "Emerald Necklace",
+      earrings: "Rose Gold Emerald Earrings",
+      bracelet: "Rose Gold Bracelet",
+    },
+
+    Blue: {
+      metal: "Silver",
+      necklace: "Sapphire Necklace",
+      earrings: "Silver Earrings",
+      bracelet: "Silver Bracelet",
+    },
+
+    White: {
+      metal: "Gold",
+      necklace: "Pearl Necklace",
+      earrings: "Pearl Earrings",
+      bracelet: "Gold Bracelet",
+    },
+  },
+};
